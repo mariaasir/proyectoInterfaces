c3013da60a96fca709de833344ffa201f9ffec8e
mariaasir <maria.l.paton@gmail.com>
2025-02-16 17:58:27 +0100

    Main para el instalador

---
+++ src/main/java/org/example/proyectointerfaces/Main.java
@@ -0,0 +1,9 @@
+package org.example.proyectointerfaces;
+
+import org.example.proyectointerfaces.InicioSesion.InicioSesionApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        InicioSesionApplication.main(args);
+    }
+}
